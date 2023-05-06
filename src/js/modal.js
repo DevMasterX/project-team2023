202241f94df7f1dac86e478af01e4e30f0b29108
DevMasterX <sokol080288@gmail.com>
2023-05-06 12:03:23 +0300

    bnh

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,14 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-  };
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-close]'),
+//     menu: document.querySelector('[data-menu]'),
+//   };
+//   refs.openMenuBtn.addEventListener('click', toggleModal);
+//   refs.closeMenuBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.menu.classList.toggle('is-hidden');
-    document.body.classList.toggle('no-scroll');
-  }
-})();
+//   function toggleModal() {
+//     refs.menu.classList.toggle('is-hidden');
+//     document.body.classList.toggle('no-scroll');
+//   }
+// })();
